ef1fa2cf311c95ea64b7f40eda6ab901100dc043
zabdielcead <zabdiel.cead@gmail.com>
2018-11-27 10:15:13 -0600

    ultimo commit directivas estructurales

--- unoholamund/src/app/components/body/body.component.ts
+++ unoholamund/src/app/components/body/body.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
+    mostrar = true;
+
+    frase: any = {
+        mensaje: 'Un gran poder requiere responsabilidad',
+        autor: 'Peter Parker'
+    };
+
+    personajes: string[] = ['Spiderman', 'Venom', 'octopus'];
+
 }
